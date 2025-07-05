67165d2f683bc4418977b9abe5c712528e0b57d6
LuizMiguelOliveira <110618848+LuizMiguelOliveira@users.noreply.github.com>
2025-07-05 17:02:02 -0300

    Add files via upload

---
+++ 01_Hello_Word.py
@@ -0,0 +1,34 @@
+print('Hello World !') # string
+print(11) # (Interio) INT
+print(True) # Booleano True e False
+print('True') # Exemplo
+print(11.07) # Float
+
+print('----------x--------------x------------')
+
+# Variável
+nome = 'Miguel'
+print(nome)
+
+# Input
+idade = input("Informe a sua idade: ")
+print(idade)
+
+
+
+print("---------------x---------------x------------------")
+
+#Teste
+nome = input("Informe seu nome: ")
+idade = int(input(" Informe sua Idade: "))
+if idade >= 18:
+    print( f"{nome} pode dirigir")
+else:
+    print(f"{nome} não pode dirigir ")
+
+
+
+
+
+
+
---
+++ atividade04.py
@@ -0,0 +1,5 @@
+temp = float(input("Escreva a temperatura em celsius: "))
+
+fahren = (temp * 1.8) + 32
+
+print(f"A temperatura em Fahrenheit e de {fahren:.2f}")
